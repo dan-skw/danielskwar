fde22d3e48c06f2a564849411ddb81b33e363d7d
dan-skw <danielrtfd@gmail.com>
2022-08-22 20:52:14 +0200

    adding livechat for work task

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contactform from "./components/Contact";
 import Footer from "./components/Footer";
+import Livechat from "./components/Livechat";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Contactform />
       <Footer />
+      <Livechat />
     </>
   );
 }
